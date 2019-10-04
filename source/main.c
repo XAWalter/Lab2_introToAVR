d20a40f87ff78ebe13bfa45f6362efecdbbd8241
Xavier Walter <xwalt001@wch136-12.cs.ucr.edu>
2019-10-04 14:10:35 -0700

    ex 1 start

--- source/main.c
+++ source/main.c
@@ -17,21 +17,23 @@ int main(void) {
 	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
 	DDRB = 0xFF; PORTB = 0x00; // Configure port B's 8 pins as outputs, initialize to 0s
 	unsigned char tmpB = 0x00; // Temporary variable to hold the value of B
-	unsigned char tmpA = 0x00; // Temporary variable to hold the value of A
+	unsigned char tmpA0 = 0x00; // Temporary variable to hold the value of A0
+	unsigned char tmpA1 = 0x00; //Temporary variable to hold the value of A1
 while(1) {
 		// 1) Read input
-		tmpA = PINA & 0x01;
+		tmpA0 = PINA & 0x01;
+		tmpA1 = PINA & 0x02;
 		// 2) Perform computation
-		// if PA0 is 1, set PB1PB0 = 01, else = 10
-		if (tmpA == 0x01) { // True if PA0 is 1
-			tmpB = (tmpB & 0xFC) | 0x01; // Sets tmpB to bbbbbb01
+		// if PA0 is 1 and PA1 is 0 then PB0 is 1 else PB0 is 0
+		if (tmpA0 == 0x01 && tmpA1 == 0x00) { // True if PA0 is 1
+			tmpB = (tmpB & 0xFE) | 0x01; // Sets tmpB to bbbbbbb1
 							 // (clear rightmost 2 bits, then set to 01)
 		} else {
-			tmpB = (tmpB & 0xFC) | 0x02; // Sets tmpB to bbbbbb10
+			tmpB = (tmpB & 0xFE) | 0x00; // Sets tmpB to bbbbbbb0
 							 // (clear rightmost 2 bits, then set to 10)
 		}	
 	// 3) Write output
-PORTB = tmpB;	
+	PORTB = tmpB;	
 	}
 	return 0;
 }
