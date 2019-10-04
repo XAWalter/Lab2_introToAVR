cb7543eb7da1b24603fdd635a184d30686665d49
Xavier Walter <xwalt001@wch136-12.cs.ucr.edu>
2019-10-04 13:08:33 -0700

    added to main.c and simAVRHeader.h TODO: GDB

--- source/main.c
+++ source/main.c
@@ -7,18 +7,22 @@
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  */
+
+
 #include <avr/io.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
-#endif
+#endif	
 
 int main(void) {
-    /* Insert DDR and PORT initializations */
-    DDRB = 0xFF;
-    PORTB = 0x00;
-    /* Insert your solution below */
-    while (1) {
-        PORTB = 0x0F;
-    }
-    return 1;
+DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
+DDRB = 0xFF; PORTB = 0x00; // Configure port B's 8 pins as outputs
+				// Initialize output on PORTB to 0x00
+unsigned char temp_input = 0x00;
+while(1) {
+	temp_input = PINA;
+	PORTB = temp_input;	// Writes port B's 8 pins with the values
+				// on port A's 8 pins
+}
+return 1;
 }
