51c1794f953fe598debf8a857ac3f180bc93db58
Xavier Walter <xwalt001@wch136-04.cs.ucr.edu>
2019-10-04 20:06:41 -0700

    added tests TODO: ALL TESTS FAIL

--- source/main.c
+++ source/main.c
@@ -17,15 +17,16 @@ int main(void) {
 	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
 	DDRC = 0xFF; PORTC = 0x00; // Configure port C's 8 pins as outputs, initialize to 0s
 	unsigned char tmpA = 0x00;	
-	unsigned char cntavail = 0x00;	
+	unsigned char cntavail = 0x04;	
+	unsigned char i = 0x00;
 
 	while(1) {
 		// 1) Read input
 		tmpA = PINA & 0x0F;	
 		// 2) Perform computation
-		for( int i = 0; i < 4; i++){
+		for(i = 0; i < 4; i++){
 			if(tmpA & 0x01){
-				cntavail++;	
+				cntavail--;	
 			}		
 			
 			tmpA = tmpA >> 1;
